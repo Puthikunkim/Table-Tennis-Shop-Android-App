025e46077db92399399fcf3e025c192d1ffdfea0
Puthikunkim <109781292+Puthikunkim@users.noreply.github.com>
2025-05-20 20:57:50 +1200

    Added ./idea to gitignore and created a basic navigation logic for the app

--- app/src/main/java/com/example/app/UI/MainActivity.java
+++ app/src/main/java/com/example/app/UI/MainActivity.java
@@ -1,24 +1,42 @@
-package com.example.app;
+package com.example.app.UI;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.example.app.R;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Initialize and assign variable
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+
+        // Set Home selected
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        // Perform item selected listener
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.cart) {
+                startActivity(new Intent(getApplicationContext(), CartActivity.class));
+                overridePendingTransition(0, 0);
+                return true;
+            } else if (id == R.id.home) {
+                return true;
+            } else if (id == R.id.search) {
+                startActivity(new Intent(getApplicationContext(), SearchActivity.class));
+                overridePendingTransition(0, 0);
+                return true;
+            }
+            return false;
         });
     }
 }
--- app/src/main/java/com/example/app/UI/SearchActivity.java
+++ app/src/main/java/com/example/app/UI/SearchActivity.java
@@ -8,35 +8,32 @@ import com.example.app.R;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class CartActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cart);
+        setContentView(R.layout.activity_search);
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
 
         // Set Home selected
-        bottomNavigationView.setSelectedItemId(R.id.cart);
+        bottomNavigationView.setSelectedItemId(R.id.search);
 
         // Perform item selected listener
         bottomNavigationView.setOnItemSelectedListener(item -> {
             int id = item.getItemId();
 
             if (id == R.id.cart) {
+                startActivity(new Intent(getApplicationContext(), CartActivity.class));
+                overridePendingTransition(0, 0);
                 return true;
             } else if (id == R.id.home) {
                 startActivity(new Intent(getApplicationContext(),MainActivity.class));
                 overridePendingTransition(0,0);
                 return true;
-            } else if (id == R.id.search) {
-                startActivity(new Intent(getApplicationContext(), SearchActivity.class));
-                overridePendingTransition(0, 0);
-                return true;
-            }
-            return false;
+            } else return id == R.id.search;
         });
     }
 }
