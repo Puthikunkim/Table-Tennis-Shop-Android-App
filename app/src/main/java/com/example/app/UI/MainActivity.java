9d2131a21551885a0c6b3b2fbd0cdeda1eb25e4e
Puthikunkim <109781292+Puthikunkim@users.noreply.github.com>
2025-05-24 18:59:05 +1200

    Implemented functionality to switch from home to details page

--- app/src/main/java/com/example/app/UI/MainActivity.java
+++ app/src/main/java/com/example/app/UI/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.app.UI;
 
+import android.content.Intent;
+import android.os.Bundle;
 import com.example.app.R;
 import com.example.app.databinding.ActivityMainBinding;
 
@@ -12,5 +14,16 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
     @Override
     protected int getSelectedMenuItemId() {
         return R.id.home;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // To test details activity
+        binding.detailsButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+            startActivity(intent);
+        });
     }
 }
