20624e279f0b980ee8a97efeae6c0e2c388a47ed
Puthikunkim <109781292+Puthikunkim@users.noreply.github.com>
2025-05-30 13:25:35 +1200

    Initialise firebase auth and add dependecy

--- app/src/main/java/com/example/app/UI/ProfileActivity.java
+++ app/src/main/java/com/example/app/UI/ProfileActivity.java
@@ -3,9 +3,14 @@ package com.example.app.UI;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import com.example.app.R;
+import com.example.app.Data.FirestoreRepository;
 import com.example.app.databinding.ActivityProfileBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends BaseActivity<ActivityProfileBinding> {
+
+    private FirebaseAuth mAuth;
+    private FirestoreRepository firestoreRepository;
 
     @Override
     protected ActivityProfileBinding inflateContentBinding() {
@@ -20,6 +25,9 @@ public class ProfileActivity extends BaseActivity<ActivityProfileBinding> {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // TODO initialize
+
+        // Initialize Firebase Auth and FirestoreRepository
+        mAuth = FirebaseAuth.getInstance();
+        firestoreRepository = FirestoreRepository.getInstance();
     }
 }
