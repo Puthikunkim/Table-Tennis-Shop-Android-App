d11720add6d6f984410c785a70120467dda2c5e2
Puthikunkim <109781292+Puthikunkim@users.noreply.github.com>
2025-05-30 13:26:01 +1200

    Wire up UI buttons

--- app/src/main/java/com/example/app/UI/ProfileActivity.java
+++ app/src/main/java/com/example/app/UI/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.example.app.UI;
 
 import android.os.Bundle;
+import android.view.View;
 import androidx.annotation.Nullable;
 import com.example.app.R;
 import com.example.app.Data.FirestoreRepository;
@@ -26,8 +27,24 @@ public class ProfileActivity extends BaseActivity<ActivityProfileBinding> {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Initialize Firebase Auth and FirestoreRepository
         mAuth = FirebaseAuth.getInstance();
         firestoreRepository = FirestoreRepository.getInstance();
+
+        // Assign listeners
+        binding.buttonSignIn.setOnClickListener(signInButtonClickListener);
+        binding.buttonCreateAccount.setOnClickListener(createAccountButtonClickListener);
+        binding.closeSignIn.setOnClickListener(closeSignInClickListener);
+        binding.closeCreate.setOnClickListener(closeCreateClickListener);
+        binding.submitSignIn.setOnClickListener(submitSignInClickListener);
+        binding.submitCreate.setOnClickListener(submitCreateClickListener);
+        binding.buttonSignOut.setOnClickListener(signOutButtonClickListener);
     }
+
+    private final View.OnClickListener signInButtonClickListener = v -> { /* TODO */ };
+    private final View.OnClickListener createAccountButtonClickListener = v -> { /* TODO */ };
+    private final View.OnClickListener closeSignInClickListener = v -> { /* TODO */ };
+    private final View.OnClickListener closeCreateClickListener = v -> { /* TODO */ };
+    private final View.OnClickListener submitSignInClickListener = v -> { /* TODO */ };
+    private final View.OnClickListener submitCreateClickListener = v -> { /* TODO */ };
+    private final View.OnClickListener signOutButtonClickListener = v -> { /* TODO */ };
 }
